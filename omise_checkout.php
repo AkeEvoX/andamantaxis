96a00cd306c8ff11cc324c83ff5ef7ccf4d7818b
AkeEvoX <svargalok@gmail.com>
2019-12-01 16:47:25 +0700

    clear code omise function

--- omise_checkout.php
+++ omise_checkout.php
@@ -1,7 +1,5 @@
 <?php
-	//require_once("/lib/omise.php");
 	require_once("/omise-php/lib/Omise.php");
-	//var_dump($_POST);
 	define("OMISE_API_VERSION","2019-05-29");
 	define("OMISE_PUBLIC_KEY","pkey_test_5ht64xcxv6dcsjtis48");
 	define("OMISE_SECRET_KEY","skey_test_5ht64xcxvqy2yfxv6nd");
@@ -11,10 +9,7 @@
 	$token=$_POST["omise_token"];
 	$orderid=$_POST["inputOrderID"];
 	$remoteAddr = $_SERVER['REMOTE_ADDR'] ;
-	//echo "<p>amount=".$amt."</p>";
-	//echo "<p>orderid=".$orderid."</p>";
-	//echo "card=".$token;
-	//$result = $omise->Charge($amt,$cur,$token);
+
 	try{
 		$result = OmiseCharge::create(array(
 			"amount" => $amt."00",
@@ -25,8 +20,6 @@
 		));
 	}
 	catch(OmiseException $ex){
-		//echo "<p>Error code : ".$ex->getCode()."</p>";
-		//echo "<p>Message :". $ex->getMessage()."</p>";
 		echo "<script>alert('Error!!, ".$ex->getMessage().".');";
 		echo "window.history.back(); ";
 		echo "</script>";
@@ -36,8 +29,7 @@
 	if($result["status"]=="successful"){
 		
 		/* redirect to omise success page */
-		
-		header("location:confirm_omise.php?id=".$orderid."&_=1w8du19162931923=");
+		header("location:confirm_omise.php?id=".$orderid."&_=".gettimeofday()['usec']);
 		exit;
 	}
 	else{
